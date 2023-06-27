e27c248f266cd91924e85fd0179d84e780b4334f
Mark Flores <mark@thegamesilo.com>
2023-06-27 17:15:19 +0800

    initialize app

---
+++ api/sync_youtube_channel.php
@@ -0,0 +1,89 @@
+<?php
+
+// Set up database connection
+$db = new mysqli('localhost', 'root', '', 'youtube_db');
+if ($db->connect_error) {
+    die('Connection failed: ' . $db->connect_error);
+}
+
+// Set API key and channel ID
+$apiKey = 'AIzaSyBbs-rrH7PH0YaW62xbb9cBNYSX2NM9G5E';
+$channelId = 'UCWJ2lWNubArHWmf3FIHbfcQ';
+
+try {
+    // Fetch channel info
+    $channelInfoUrl = 'https://www.googleapis.com/youtube/v3/channels?part=snippet&id=' . $channelId . '&key=' . $apiKey;
+    $channelData = json_decode(file_get_contents($channelInfoUrl), true);
+
+    $channelProfilePic = $channelData['items'][0]['snippet']['thumbnails']['default']['url'];
+    $channelName = $channelData['items'][0]['snippet']['title'];
+    $channelDescription = $channelData['items'][0]['snippet']['description'];
+
+    // Check if channel already exists in the database
+    $existingChannelStmt = $db->prepare('SELECT id FROM youtube_channels WHERE name = ?');
+    $existingChannelStmt->bind_param('s', $channelName);
+    $existingChannelStmt->execute();
+    $existingChannelStmt->store_result();
+    $numRows = $existingChannelStmt->num_rows;
+    $existingChannelStmt->close();
+
+    if ($numRows === 0) {
+        // Insert channel info into the database
+        $insertChannelStmt = $db->prepare('INSERT INTO youtube_channels (profile_picture, name, description) VALUES (?, ?, ?)');
+        $insertChannelStmt->bind_param('sss', $channelProfilePic, $channelName, $channelDescription);
+        $insertChannelStmt->execute();
+        $lastInsertedId = $insertChannelStmt->insert_id;
+        $insertChannelStmt->close();
+
+        $videoInfoUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' . $channelId . '&maxResults=50&order=date&key=' . $apiKey;
+
+        $videoData = [];
+        $nextPageToken = '';
+        $totalVideos = 0;
+        
+        while ($totalVideos < 100 && $nextPageToken !== null) {
+            $remainingVideos = 100 - $totalVideos;
+            $maxResults = min(50, $remainingVideos);
+        
+            $videoInfoUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' . $channelId . '&maxResults=' . $maxResults . '&order=date&key=' . $apiKey;
+        
+            if ($nextPageToken !== '') {
+                $videoInfoUrl .= '&pageToken=' . $nextPageToken;
+            }
+        
+            $response = file_get_contents($videoInfoUrl);
+            $data = json_decode($response, true);
+            $videoData = array_merge($videoData, $data['items']);
+            $nextPageToken = isset($data['nextPageToken']) ? $data['nextPageToken'] : null;
+        
+            $totalVideos += $maxResults;
+        }
+        
+        // Insert video info into the database
+        $insertVideoStmt = $db->prepare('INSERT INTO youtube_channel_videos (channel_id, video_link, title, description, thumbnail) VALUES (?, ?, ?, ?, ?)');
+        foreach ($videoData as $video) {
+            $videoChannelId = $lastInsertedId;
+            $videoLink = 'https://www.youtube.com/watch?v=' . $video['id']['videoId'];
+            $videoTitle = $video['snippet']['title'];
+            $videoDescription = $video['snippet']['description'];
+            $videoThumbnail = $video['snippet']['thumbnails']['high']['url'];
+        
+            $insertVideoStmt->bind_param('sssss', $videoChannelId, $videoLink, $videoTitle, $videoDescription, $videoThumbnail);
+            $insertVideoStmt->execute();
+        }
+        $insertVideoStmt->close();        
+    }
+
+    echo json_encode([
+        'name' => $channelName,
+        'description' => $channelDescription,
+        'profile_pic' => $channelProfilePic,
+    ]);
+
+    $db->close();
+} catch (Exception $e)
+{
+    echo json_encode([
+        'error' => $e->getMessage(),
+    ]);
+}
---
+++ api/youtube_channel_json.php
@@ -0,0 +1,50 @@
+<?php
+
+    function get_db_connection() {
+        $db = new mysqli('localhost', 'root', '', 'youtube_db');
+        if ($db->connect_error) {
+            die('Connection failed: ' . $db->connect_error);
+        }
+
+        return $db;
+    }
+
+    function get_channel_info($db) {
+        $channelInfoStmt = $db->prepare('SELECT * FROM youtube_channels');
+        $channelInfoStmt->execute();
+        $channelInfoResult = $channelInfoStmt->get_result();
+        $channelInfo = $channelInfoResult->fetch_all(MYSQLI_ASSOC);
+        $channelInfoStmt->close();
+
+        return $channelInfo;
+    }
+
+    function get_channel_videos($db, $channelId) {
+        $videosStmt = $db->prepare('SELECT youtube_channel_videos.*, youtube_channels.profile_picture FROM youtube_channel_videos 
+        INNER JOIN youtube_channels ON youtube_channels.id = youtube_channel_videos.channel_id WHERE channel_id = ?');
+        $videosStmt->bind_param('s', $channelId);
+        $videosStmt->execute();
+        $videosResult = $videosStmt->get_result();
+        $videos = $videosResult->fetch_all(MYSQLI_ASSOC);
+        $videosStmt->close();
+
+        return $videos;
+    }
+
+    $db = get_db_connection();
+    $channelId = $_GET['channelId'];
+
+    if (isset($channelId)) {
+        $videos = get_channel_videos($db, $channelId);
+        $response = ['videos' => $videos];
+    } else {
+        $channelInfo = get_channel_info($db);
+        $response = ['channelInfo' => $channelInfo];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+
+    $db->close();
+
+?>
